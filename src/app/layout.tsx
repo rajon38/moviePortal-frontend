025f1eeff69fdfed7c6c4fb4fc53b09390f11313
rashedul islam <rashedulislam@rasheduls-MacBook-Pro.local>
2026-03-29 20:36:19 +0600

    implement authentication and cookie management with axios client

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import QueryProviders from "@/providers/QueryProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,12 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <QueryProviders>
+          {children}
+          <Toaster position="top-right" richColors />
+        </QueryProviders>
+        </body>
     </html>
   );
 }
